1a94c412843749c8c51d09bb18a05d4970f37ce7
cheton <cheton@gmail.com>
2024-07-06 00:16:03 +0800

    feat: add `subtract` function

--- packages/monorepo-sandbox-y/lib/index.js
+++ packages/monorepo-sandbox-y/lib/index.js
@@ -1,4 +1,4 @@
-const { add } = require('@cheton/monorepo-sandbox-x');
+const { add, subtract } = require('@cheton/monorepo-sandbox-x');
 
 const multiply = (x, y) => {
   return x * y;
@@ -6,5 +6,6 @@ const multiply = (x, y) => {
 
 module.exports = {
   add,
+  subtract,
   multiply,
 };
