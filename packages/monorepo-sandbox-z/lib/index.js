eff62e39feb92fe278989eefe3f13422591ff2fc
cheton <cheton@gmail.com>
2024-07-05 14:09:40 +0800

    feat: export the divide function

--- packages/monorepo-sandbox-z/lib/index.js
+++ packages/monorepo-sandbox-z/lib/index.js
@@ -1,1 +1,12 @@
-module.exports = "mono-three";
+const { add } = require('@cheton/monorepo-sandbox-x');
+const { multiply } = require('@cheton/monorepo-sandbox-y');
+
+const divide = (x, y) => {
+  return x / y;
+};
+
+module.exports = {
+  add,
+  multiply,
+  divide,
+};
